30c6def214cc5571866672763198123fa811b325
Folmagaut <folm87@gmail.com>
2024-09-18 00:42:24 +0300

    Тернистый путь

--- blocknote.cpp
+++ blocknote.cpp
@@ -1,35 +1,131 @@
-#include <array>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <string>
 
 using namespace std;
+using filesystem::path;
 
-int main(int argc, const char** argv) {
-    // при неверных аргументах выводим ошибку и выходим с кодом
-    if (argc != 3) {
-        cerr << "Usage: "s << argv[0] << " <in file> <out file>"s << endl;
-        return 1;
-    }
-
-    ifstream in_file(argv[1], ios::in | ios::binary);
-    if (!in_file) {
-        cerr << "Can't open input file"s << endl;
-        return 2;
-    }
-
-    ofstream out_file(argv[2], ios::out | ios::binary);
-    if (!out_file) {
-        cerr << "Can't open output file"s << endl;
-        return 2;
-    }
-
-    // размер буфера один килобайт
-    static const int BUFF_SIZE = 1024;
-    std::array<char, BUFF_SIZE> buffer;
-
-    do {
-        in_file.read(buffer.data(), BUFF_SIZE);
-        out_file.write(buffer.data(), in_file.gcount());
-    } while (in_file);
+path operator""_p(const char* data, std::size_t sz) {
+    return path(data, data + sz);
 }
+
+void CreateFile(path p) {
+    ofstream file(p);
+    if (file) {
+        cout << "Файл создан: "s << p.string() << endl;
+    } else {
+        cout << "Ошибка создания файла: "s << p.string() << endl;
+    }
+}
+
+void PrintFileOrFolder(filesystem::file_status status) {
+    if (status.type() == filesystem::file_type::regular) {
+        cout << "Путь указывает на файл"s << endl;
+    } else if (status.type() == filesystem::file_type::directory) {
+        cout << "Путь указывает на папку"s << endl;
+    } else {
+        cout << "Путь указывает другой объект"s << endl;
+    }
+}
+void PrintFileOrFolder(filesystem::path p) {
+    error_code err;
+    auto status = filesystem::status(p, err);
+    
+    if (err) {
+        return;
+    }
+
+    PrintFileOrFolder(status);
+}
+
+int main() {
+    path p = "a"_p / "folder"_p;
+    filesystem::create_directories(p);
+    filesystem::create_directory(p / "subfolder"_p);
+    std::ofstream(p / "file.txt"_p) << "File content"s;
+
+    for (const auto& dir_entry: filesystem::directory_iterator(p)) {
+        PrintFileOrFolder(dir_entry.status());
+    }
+}
+
+/*
+#include <cassert>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+using filesystem::path;
+
+path operator""_p(const char* data, std::size_t sz) {
+    return path(data, data + sz);
+}
+
+ struct PathInfo {
+    bool isDirectory;
+    std::string name;
+};
+
+void PrintTree(ostream& dst, const path& p, const filesystem::file_status& status, int offset) {
+    //error_code err;
+    offset += 2;
+    //auto status = filesystem::status(p, err);
+    for (const auto& dir_entry: filesystem::directory_iterator(p)) {
+        if (status.type() == filesystem::file_type::directory) {
+            PrintTree(dst, dir_entry.path(), status, offset);
+        }
+    }
+}
+
+// напишите эту функцию
+void PrintTree(ostream& dst, const path& p) {
+    
+    vector<string> out_to_dst;
+    out_to_dst.push_back(p.string());
+    dst << p.string() << endl;
+
+    for (const auto& dir_entry: filesystem::directory_iterator(p)) {
+        //error_code err;
+        //auto status = filesystem::status(dir_entry.path(), err);
+        if (dir_entry.status().type() == filesystem::file_type::directory) {
+            out_to_dst.push_back(dir_entry.path().filename().string());
+
+            for (const auto& dir_entry : filesystem::directory_iterator(dir_entry.path())) {
+                out_to_dst.push_back(dir_entry.path().filename().string());
+            }
+
+        } else if (status.type() == filesystem::file_type::regular) {
+            out_to_dst.push_back(dir_entry.path().filename().string());
+        }
+    }
+    for (auto s : out_to_dst) {
+        cout << s << endl;
+    }
+}
+
+int main() {
+    error_code err;
+    filesystem::remove_all("test_dir", err);
+    filesystem::create_directories("test_dir"_p / "a"_p, err);
+    filesystem::create_directories("test_dir"_p / "b"_p, err);
+
+    ofstream("test_dir"_p / "b"_p / "f1.txt"_p);
+    ofstream("test_dir"_p / "b"_p / "f2.txt"_p);
+    ofstream("test_dir"_p / "a"_p / "f3.txt"_p);
+
+    ostringstream out;
+    PrintTree(out, "test_dir"_p);
+    /* assert(out.str() ==
+        "test_dir\n"
+        "  b\n"
+        "    f2.txt\n"
+        "    f1.txt\n"
+        "  a\n"
+        "    f3.txt\n"s
+    );
+}
+*/
--- tests.cpp
+++ tests.cpp
@@ -1,282 +1,87 @@
+#include <algorithm>
+#include <cassert>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
-#include <vector>
+#include <sstream>
 #include <string>
-#include <iterator>
- 
-using namespace std;
- 
-struct Document {
-    Document() = default;
-    Document(int id, double relevance, int rating)
-        : id(id)
-        , relevance(relevance)
-        , rating(rating) {
-    }
-    int id = 0;
-    double relevance = 0.0;
-    int rating = 0;
-};
- 
-template <typename Iterator>
-class Paginator {
-public:
-    Paginator(Iterator first, Iterator last, size_t page_size) {
-        while (first != last) {
-            Iterator current_end = first;
-            advance(current_end, min(page_size, static_cast<size_t>(distance(first, last))));
-            pages_.push_back(make_pair(first, current_end));
-            first = current_end;
-        }
-    }
- 
-    auto begin() const {
-        return pages_.begin();
-    }
- 
-    auto end() const {
-        return pages_.end();
-    }
- 
-    size_t size() const {
-        return pages_.size();
-    }
- 
-private:
-    vector<pair<Iterator, Iterator>> pages_;
-};
- 
-template <typename Container>
-auto Paginate(const Container& c, size_t page_size) {
-    return Paginator(begin(c), end(c), page_size);
-}
- 
-ostream& operator<<(ostream& output, const Document& document) {
-    return output << "{ document_id = "s << document.id
-                  << ", relevance = "s << document.relevance
-                  << ", rating = "s << document.rating << " }"s;
-}
- 
-template <typename Iterator>
-ostream& operator<<(ostream& output, const pair<Iterator, Iterator>& page) {
-    for (auto it = page.first; it != page.second; ++it) {
-        output << *it;
-    }
-    return output;
-}
- 
-int main() {
-    vector<Document> search_results;
- 
-    for (int i = 0; i < 8; ++i) {
-        Document doc(i, i, i);
-        search_results.push_back(doc);
-    }
-    int page_size = 3;
-    const auto pages = Paginate(search_results, page_size);
- 
-    // Выводим найденные документы по страницам
-    for (const auto& page : pages) {
-        cout << page << endl;
-        cout << "Page break"s << endl;
-    }
-}
-
-/*
-/////////////////////////////////////////////////
-template <typename Iterator>
-class IteratorRange {
-public:
-    IteratorRange(Iterator begin, Iterator end)
-        : first_(begin)
-        , last_(end)
-        , size_(distance(first_, last_)) {
-    }
-
-    Iterator begin() const {
-        return first_;
-    }
-
-    Iterator end() const {
-        return last_;
-    }
-
-    size_t size() const {
-        return size_;
-    }
-
-private:
-    Iterator first_, last_;
-    size_t size_;
-};
-
-template <typename Iterator>
-ostream& operator<<(ostream& out, const IteratorRange<Iterator>& range) {
-    for (Iterator it = range.begin(); it != range.end(); ++it) {
-        out << *it;
-    }
-    return out;
-}
-
-template <typename Iterator>
-class Paginator {
-public:
-    Paginator(Iterator begin, Iterator end, size_t page_size) {
-        for (size_t left = distance(begin, end); left > 0;) {
-            const size_t current_page_size = min(page_size, left);
-            const Iterator current_page_end = next(begin, current_page_size);
-            pages_.push_back({begin, current_page_end});
-
-            left -= current_page_size;
-            begin = current_page_end;
-        }
-    }
-
-    auto begin() const {
-        return pages_.begin();
-    }
-
-    auto end() const {
-        return pages_.end();
-    }
-
-    size_t size() const {
-        return pages_.size();
-    }
-
-private:
-    vector<IteratorRange<Iterator>> pages_;
-};
-
-template <typename Container>
-auto Paginate(const Container& c, size_t page_size) {
-    return Paginator(begin(c), end(c), page_size);
-}
-
-//////////////////////////////////////////
-#include <iostream>
-#include <iterator>
 #include <vector>
-#include <string>
 
 using namespace std;
+using filesystem::path;
 
-struct Document {
-    Document() = default;
-    Document(int id, double relevance, int rating)
-        : id(id)
-        , relevance(relevance)
-        , rating(rating) {
-    }
-    int id = 0;
-    double relevance = 0.0;
-    int rating = 0;
+path operator""_p(const char* data, std::size_t sz) {
+    return path(data, data + sz);
+}
+
+struct PathInfo {
+    bool isDirectory;
+    std::string name;
 };
 
-
-template <typename Iterator>
-class Paginator {
-public:
-    //Paginator() = default;
-    
-    Paginator(Iterator first, Iterator last, size_t page_size) : m_page_size_(page_size), vec_pages_(MakeVec(first, last)) {
+bool comparePathInfos(const PathInfo& lhs, const PathInfo& rhs) {
+    // Используем функцию сравнения строковых значений, но только если они оба являются файлами
+    if (!lhs.isDirectory && !rhs.isDirectory) {
+        return lhs.name > rhs.name;
     }
-    vector<vector<Iterator>> MakeVec(Iterator begin, Iterator end) {
-        vector<vector<Iterator>> vec;
-        Iterator iter = begin;
-        auto dist = distance(begin, end);
-        if (distance(begin, end) <= m_page_size_) {
-            vector<Iterator> single_page_vec = {};
-            for (auto it = begin; it != end; ++it) {
-                single_page_vec.push_back(it);
-            }
-            vec.push_back(single_page_vec);
-            return vec;
-        } else {
-            vector<Iterator> single_page_vec = {};
-            for (auto it = begin; it != end; ++it) {
-                //vector<Iterator> single_page_vec = {};
-                if (it < (iter + m_page_size_)) {
-                    single_page_vec.push_back(it);
-                    if (it == end - 1) {
-                        vec.push_back(single_page_vec);
-                        return vec;
-                    }
-                    continue;
-                } else {
-                    vec.push_back(single_page_vec);
-                    single_page_vec = {};
-                    advance(iter, m_page_size_);
-                }
-            }
-            //vec_pages_ = vec;
-            return vec;
-        }
-    }
-
-    auto begin() const {
-        return vec_pages_.begin();
-    }
-
-    auto end() const {
-        return vec_pages_.end();
-    }
-
-private:
-    size_t m_page_size_;
-    //pair<Iterator, Iterator> m_pages_;
-    vector<vector<Iterator>> vec_pages_;
-};
-
-template <typename Container>
-auto Paginate(const Container& c, size_t page_size) {
-    return Paginator(begin(c), end(c), page_size);
+    // В противном случае сортируем по типу (папка всегда выше файла)
+    return lhs.isDirectory || rhs.isDirectory ? lhs.isDirectory : false;
 }
 
-ostream& operator<<(ostream& output, const Document& document) {
-    return output << "{ document_id = "s << document.id << ", relevance = "s << document.relevance << ", rating = "s << document.rating << " }"s;
-}
-
-template <typename Iterator>
-ostream& operator<<(ostream& output, const Paginator<vector<vector<Iterator>>>& paginator) {
-    
-    for (size_t i = 0; i < paginator.vec_pages_.size(); ++i) {
-        for (const auto page : paginator.vec_pages_[i]) {
-            //for (auto it = paginator.vec_pages_[i].begin(); it != paginator.vec_pages_[i].end(); ++it) {
-                output << *page;
-            //}
+void PrintTree(ostream& dst, const path& p, int offset) {
+    //dst << p.string() << endl;
+    error_code err;
+    auto status = filesystem::status(p, err);
+    offset += 2;
+    vector<string> elements;
+    if (status.type() == filesystem::file_type::directory) {
+        for (const auto& dir_entry: filesystem::directory_iterator(p)) {
+        elements.push_back(dir_entry.path().filename().string());
         }
     }
-        
-    return output;
-}
+    sort(elements.begin(), elements.end(), [] (string lhs, string rhs) {
+        return lhs > rhs;
+    });
 
-template <typename Iterator>
-ostream& operator<<(ostream& output, const vector<vector<Iterator>>& vec_pages) {
-    
-    for (size_t i = 0; i < vec_pages.size(); ++i) {
-        for (const auto page : vec_pages[i]) {
-            //for (auto it = paginator.vec_pages_[i].begin(); it != paginator.vec_pages_[i].end(); ++it) {
-                output << *page;
-            //}
+    for (const auto& s : elements) {
+        path pth(p / s);
+        auto status = filesystem::status(pth, err);
+        if (status.type() == filesystem::file_type::directory) {
+            dst << string(offset, ' ') << s << endl;
+            PrintTree(dst, pth, offset);
+        }
+        else {
+            dst << string(offset, ' ') << s << endl;
         }
     }
-        
-    return output;
+}
+
+// напишите эту функцию
+void PrintTree(ostream& dst, const path& p) {
+    dst << p.string() << endl;
+    int offset = 0;
+    PrintTree(dst, p, offset);
 }
 
 int main() {
-    vector<Document> search_results;
-    
-    for (int i = 0; i < 5; ++i) {
-        Document doc(i, i, i);
-        search_results.push_back(doc);
-    }
-    size_t page_size = 2;
-    //cout << search_results.size() << endl;
-    const auto pages = Paginate(search_results, page_size);
-    // Выводим найденные документы по страницам
-    for (auto page = pages.begin(); page != pages.end(); ++page) {
-        cout << *page << endl;
-        cout << "Page break"s << endl;
-    }
-} */
+    error_code err;
+    filesystem::remove_all("test_dir", err);
+    filesystem::create_directories("test_dir"_p / "a"_p, err);
+    filesystem::create_directories("test_dir"_p / "b"_p, err);
+
+    ofstream("test_dir"_p / "b"_p / "f1.txt"_p);
+    ofstream("test_dir"_p / "b"_p / "f2.txt"_p);
+    ofstream("test_dir"_p / "a"_p / "f3.txt"_p);
+
+    ostringstream out;
+    PrintTree(out, "test_dir"_p);
+    cout << out.str();
+    /* assert(out.str() ==
+        "test_dir\n"
+        "  b\n"
+        "    f2.txt\n"
+        "    f1.txt\n"
+        "  a\n"
+        "    f3.txt\n"s
+    ); */
+}
